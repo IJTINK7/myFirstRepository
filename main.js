9ce5b8b169725e257ada560d1a5ed8471f564eb3
IJTINK <ijtink7@mail.ru>
2022-10-30 13:13:49 +0300

    Adding 2nd Task

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 //Hello, World
 
-function greet(name){
-    return `Hello, ${name} how are you doing today?`;
-}
-console.log(greet('Sarah'));
+// function greet(name){
+//     return `Hello, ${name} how are you doing today?`;
+// }
+// console.log(greet('Sarah'));
+
+// String.prototype.isUpperCase = function() {
+//     if (this.toUpperCase() == this) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
