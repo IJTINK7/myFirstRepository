5946707f57e9a36426a495984c90f8f117a3f30a
IJTINK <ijtink7@mail.ru>
2022-11-02 02:41:50 +0300

    Completed 10th Task

--- Training JS.js
+++ Training JS.js
@@ -111,3 +111,15 @@
 //       return result = "*".repeat((n+1)/2) + str + "*".repeat(n/2);
 //    }
 //  }
+// #10
+// function pickIt(arr){
+//     var odd=[],even=[];
+//     for(let i = 0; i < arr.length; i++){
+//       if(arr[i] % 2){
+//         odd.push(arr[i]);
+//       }else{
+//         even.push(arr[i]);
+//       }
+//     }
+//     return [odd,even];
+// }
