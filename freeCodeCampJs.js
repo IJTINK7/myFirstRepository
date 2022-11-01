2e1386cb3f255c6d4bd9c49f6f0831c7aacdd0d7
IJTINK <ijtink7@mail.ru>
2022-11-01 20:08:15 +0300

    Adding 93 Task

--- freeCodeCampJs.js
+++ freeCodeCampJs.js
@@ -235,3 +235,16 @@
 //       ],
 //     }
 // ];
+// #93
+// const myStorage = {
+//     "car": {
+//       "inside": {
+//         "glove box": "maps",
+//         "passenger seat": "crumbs"
+//        },
+//       "outside": {
+//         "trunk": "jack"
+//       }
+//     }
+//   };
+// const gloveBoxContents = myStorage.car.inside["glove box"];
