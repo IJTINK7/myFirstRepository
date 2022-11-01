fcfc332398458f0f7ec97d6952f3fb19a5889d65
IJTINK <ijtink7@mail.ru>
2022-11-01 11:37:22 +0300

    Completed 8th Task

--- Training JS.js
+++ Training JS.js
@@ -94,3 +94,14 @@
 //       return n * 90;
 //     }
 // }
+// #8 Best Practice
+// function howManydays(month){
+//     switch (month){
+//        case 2: return 28
+//        case 4:
+//        case 6:
+//        case 9:
+//        case 11: return 30
+//     }
+//     return 31
+// }
