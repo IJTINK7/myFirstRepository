4cb439e1463581571b7b29eb7b986b1559879b2f
IJTINK <ijtink7@mail.ru>
2022-11-01 21:49:42 +0300

    Adding 110 Task

--- freeCodeCampJs.js
+++ freeCodeCampJs.js
@@ -390,3 +390,8 @@
 //     return parseInt(str, 2);
 // }
 // convertToInteger("10011");
+// #110
+// function checkEqual(a, b) {
+//     return a === b ? "Equal" : "Not Equal";
+// }
+// checkEqual(1, 2);
