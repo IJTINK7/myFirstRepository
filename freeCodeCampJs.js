dada94e52d855000903ab4479269b160d85f656f
IJTINK <ijtink7@mail.ru>
2022-11-01 18:51:07 +0300

    Adding 85 Task

--- freeCodeCampJs.js
+++ freeCodeCampJs.js
@@ -147,3 +147,11 @@
 //   };
 //   const hatValue = testObj.hat;
 //   const shirtValue = testObj.shirt;
+// 85#
+// const testObj = {
+//     "an entree": "hamburger",
+//     "my side": "veggies",
+//     "the drink": "water"
+//   };
+//   const entreeValue = testObj["an entree"];
+//   const drinkValue = testObj["the drink"];
