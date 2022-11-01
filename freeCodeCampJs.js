76129526a27e3f6409ea8b2382911f1fd9197e73
IJTINK <ijtink7@mail.ru>
2022-11-01 21:47:59 +0300

    Adding 109 Task

--- freeCodeCampJs.js
+++ freeCodeCampJs.js
@@ -384,6 +384,9 @@
 // function convertToInteger(str) {
 //     return parseInt(str);
 // }
-  
 // convertToInteger("56");
-
+// #109
+// function convertToInteger(str) {
+//     return parseInt(str, 2);
+// }
+// convertToInteger("10011");
