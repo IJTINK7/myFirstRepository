2fb469a157da19d6ebdc0b9c1e9df2c5673766db
IJTINK <ijtink7@mail.ru>
2022-11-01 10:40:40 +0300

    Adding 15th Task

--- main.js
+++ main.js
@@ -72,3 +72,31 @@
 // function solution(str, ending){
 //   return str.endsWith(ending);
 // }
+// 15 Return the Day
+// function whatday(num) { 
+//     switch(num) {
+//       case 1:
+//         return 'Sunday';
+//         break;
+//       case 2:
+//         return 'Monday';
+//         break;
+//       case 3:
+//         return 'Tuesday';
+//         break;
+//       case 4:
+//         return 'Wednesday';
+//         break;
+//       case 5:
+//         return 'Thursday';
+//         break;
+//       case 6:
+//         return 'Friday';
+//         break;
+//       case 7:
+//         return 'Saturday';
+//         break;
+//       default:
+//         return  "Wrong, please enter a number between 1 and 7"
+//     }
+// }
