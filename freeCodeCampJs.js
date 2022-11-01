b9fa385d174bd79f291ed2ca88836e1383653482
IJTINK <ijtink7@mail.ru>
2022-11-01 18:43:27 +0300

    Adding 82 Task

--- freeCodeCampJs.js
+++ freeCodeCampJs.js
@@ -116,3 +116,20 @@
 //     return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 //   }
 // abTest(2,2);
+#82
+// let count = 0;
+// function cc(card) {
+//   if((card == 2) || (card == 3) || (card == 4) || (card == 5) || (card == 6)){
+//     count += 1;
+//   }else if((card == 7) || (card == 8) || (card == 9)){
+//     count += 0;
+//   }else if((card == 10) || (card == "J") || (card == "Q") || (card == "K") || (card == "A")){
+//     count -= 1;
+//   }
+//   if(count > 0){
+//     return `${count} Bet`;
+//   }else{
+//     return `${count} Hold`;
+//   }
+// }
+// cc(2); cc(3); cc(7); cc('K'); cc('A');
