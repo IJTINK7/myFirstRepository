92c923994c981fdeb13d7015c6288756c884be4f
IJTINK <ijtink7@mail.ru>
2022-11-02 03:07:42 +0300

    Completed 12th Task

--- Training JS.js
+++ Training JS.js
@@ -137,3 +137,17 @@
 //     return bag;
 // }
 
+// #12
+// function giveMeFive(obj){
+//     let arr = [];
+//     for (let key in obj){
+//       if(key.length == 5){
+//         arr.push(key);
+//       }
+//       if(obj[key].length == 5){
+//         arr.push(obj[key]);
+//       }
+//     }
+//     return arr;
+// }
+
