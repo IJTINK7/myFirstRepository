7fc28ffe2d362461aeafebda433d6bb1e2707f5a
IJTINK7 <ijtink7@mail.ru>
2022-11-27 20:04:23 +0300

    Formatting 18th Task

--- main.js
+++ main.js
@@ -117,11 +117,9 @@
 
 // 18 Find Maximum and Minimum Values of a List
 // let min = function(list){
-//
 //     return Math.min(...list);
 // }
 //
 // let max = function(list){
-//
 //     return Math.max(...list);
 // }
