c4abecec7e7137910a054be46c1ef9af73b94f62
IJTINK7 <ijtink7@mail.ru>
2023-02-05 22:41:13 +0300

    Creating new object, using this values

--- src/Mutability/Mutability.tsx
+++ src/Mutability/Mutability.tsx
@@ -3,11 +3,23 @@ let user = {
 	name: "Roma",
 	age: 27
 }
+let newUser = user
+let admins = [
+	user,
+	{
+		name: "Anton",
+		age: 24
+	}
 
+]
+console.log(admins[1])
 export const Mutability=()=>{
+
 	return (
 		<div>
-			{user.age = user.age + 1}
+			<div>{user.age = user.age + 1}</div>
+			<div>{newUser.age =  28}</div>
+			<div>{user.age}</div>
 		</div>
 	)
 }
