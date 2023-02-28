a8dc980cf56b63141765eeba99846fc1db8d1f76
IJTINK7 <ijtink7@mail.ru>
2023-02-28 23:22:03 +0300

    Adding 62nd Task

--- src/Files/MainJS.tsx
+++ src/Files/MainJS.tsx
@@ -373,6 +373,17 @@ export const MainJS =()=>{
 // 	function countSheeps(arrayOfSheeps) {
 // 		return arrayOfSheeps.filter(Boolean).length;
 // 	}
+// 	62 Switch/Case - Bug Fixing #6
+// 	function evalObject(value){
+// 		switch(value.operation){
+// 			case'+': return value.a + value.b;
+// 			case'-': return value.a - value.b;
+// 			case'/': return value.a / value.b;
+// 			case'*': return value.a * value.b;
+// 			case'%': return value.a % value.b;
+// 			case'^': return Math.pow(value.a, value.b);
+// 		}
+// 	}
 
 	return(
 		<div>MainJS</div>
