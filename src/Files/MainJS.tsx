59c69359fa8b8dd6569ebe80cbed0b3feea33699
IJTINK7 <ijtink7@mail.ru>
2023-03-26 22:35:14 +0300

    Adding 82nd Task

--- src/Files/MainJS.tsx
+++ src/Files/MainJS.tsx
@@ -478,6 +478,11 @@ export const MainJS =()=>{
 	// 	return Math.floor(s / 0.036);
 	// }
 //
+// 	82 Beginner Series #1 School Paperwork
+// 	function paperwork(n, m) {
+// 		return n > 0 && m > 0 ? n * m : 0;
+// 	}
+
 	return(
 		<div>MainJS</div>
 	);
