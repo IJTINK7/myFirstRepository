e1161f6fb55c61824a005e54d46f694293e4e859
IJTINK7 <ijtink7@mail.ru>
2023-03-29 22:21:07 +0300

    Adding 86th Task

--- src/Files/MainJS.tsx
+++ src/Files/MainJS.tsx
@@ -497,6 +497,14 @@ export const MainJS =()=>{
 	// function typeValidation(variable, type) {
 	// 	return (typeof variable) === type
 	// }
+	// 86 What is between?
+	// 	function between(a, b) {
+	// 		let arr = [];
+	// 		for (let i = a; i <= b; i++){
+	// 			arr.push(i)
+	// 		}
+	// 		return arr;
+	// 	}
 
 	return(
 		<div>MainJS</div>
