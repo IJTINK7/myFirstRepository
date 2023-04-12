a5f19454aa7f26db592f85197d15c4c70ba24750
IJTINK7 <ijtink7@mail.ru>
2023-04-12 23:24:30 +0300

    Adding 96th Task

--- src/Files/MainJS.tsx
+++ src/Files/MainJS.tsx
@@ -539,6 +539,18 @@ export const MainJS =()=>{
 	// 	function hero(bullets, dragons){
 	// 		return bullets >= dragons * 2
 	// 	}
+	// 96 Rock Paper Scissors!
+	// const rps = (p1, p2) => {
+	// 	if (p1 === 'rock' && p2 === 'scissors') return "Player 1 won!";
+	// 	if (p1 === 'scissors' && p2 === 'paper') return "Player 1 won!";
+	// 	if (p1 === 'paper' && p2 === 'rock') return "Player 1 won!";
+	// 	if (p1 === 'rock' && p2 === 'paper') return "Player 2 won!";
+	// 	if (p1 === 'scissors' && p2 === 'rock') return "Player 2 won!";
+	// 	if (p1 === 'paper' && p2 === 'scissors') return "Player 2 won!";
+	// 	if (p1 === 'rock' && p2 === 'rock') return "Draw!";
+	// 	if (p1 === 'scissors' && p2 === 'scissors') return "Draw!";
+	// 	if (p1 === 'paper' && p2 === 'paper') return "Draw!";
+	// };
 
 	return(
 		<div>MainJS</div>
