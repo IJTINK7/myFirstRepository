ef22cac7c94224a49ddfb4cf0a9e6d2b49629d5a
IJTINK7 <ijtink7@mail.ru>
2023-05-05 21:58:38 +0300

    Adding 105th Task

--- src/Files/MainJS.tsx
+++ src/Files/MainJS.tsx
@@ -587,6 +587,27 @@ export const MainJS =()=>{
 	// 	function expressionMatter(a, b, c) {
 	// 		return Math.max( a + b + c, (a + b) * c, a * (b + c), a * b * c ) ;
 	// 	}
+	105 Get Planet Name By ID
+	function getPlanetName(id){
+		switch(id){
+			case 1:
+				return'Mercury'
+			case 2:
+				return'Venus'
+			case 3:
+				return'Earth'
+			case 4:
+				return'Mars'
+			case 5:
+				return'Jupiter'
+			case 6:
+				return 'Saturn'
+			case 7:
+				return 'Uranus'
+			case 8:
+				return'Neptune'
+		}
+	}
 
 	return(
 		<div>MainJS</div>
