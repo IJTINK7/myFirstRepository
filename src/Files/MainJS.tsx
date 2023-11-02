f71db90206e5aa80281b8be0492611f85cf0f7bb
IJTINK7 <ijtink7@mail.ru>
2023-11-02 22:44:33 +0300

    Adding 133rd Task

--- src/Files/MainJS.tsx
+++ src/Files/MainJS.tsx
@@ -761,6 +761,13 @@ export const MainJS = () => {
 	// 	}
 	// 	return pontoonDistance / youSpeed < sharkDistance / sharkSpeed ? "Alive!" : "Shark Bait!";
 	// }
+	// 133 Online RPG: player to qualifying stage?
+	// function playerRankUp (points) {
+	// 	if (points >= 100){
+	// 		return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+	// 	}
+	// 	return false
+	// }
 	return (
 		<div>MainJS</div>
 	);
