01738b076c086bc6c9d83f72e965df8f04b85c0b
IJTINK7 <ijtink7@mail.ru>
2023-11-04 16:50:24 +0300

    Adding 135th Task

--- src/Files/MainJS.tsx
+++ src/Files/MainJS.tsx
@@ -772,6 +772,10 @@ export const MainJS = () => {
 	// function odds(values){
 	// 	return values.filter(el => el % 2 != 0 );
 	// }
+	// 135 Hello, Name or World!
+	// function hello(name) {
+	// 	return `Hello, ${name ? name[0].toUpperCase() + name.slice(1).toLowerCase() : "World"}!`
+	// }
 	return (
 		<div>MainJS</div>
 	);
