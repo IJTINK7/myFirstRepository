4269a7f32116f41faafa840d3859c65c8daee7c2
IJTINK7 <ijtink7@mail.ru>
2023-11-09 22:30:28 +0300

    Adding 140th Task

--- src/Files/MainJS.tsx
+++ src/Files/MainJS.tsx
@@ -796,6 +796,22 @@ export const MainJS = () => {
 	// function power(x,y){
 	// 	return x ** y;
 	// }
+	// 140	8kyu interpreters: HQ9+
+	// function HQ9(code) {
+	// 	if(code === 'H') return 'Hello World!'
+	// 	if(code === 'Q') return code
+	// 	let result = ''
+	// 	if(code === '9'){
+	// 		for(let i = 99; i>2;i--){
+	// 			result +=  `${i} bottles of beer on the wall, ${i} bottles of beer.\nTake one down and pass it around, ${i-1} bottles of beer on the wall.\n`
+	// 		}
+	// 		result += `2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n`
+	// 		result += `1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n`
+	// 		result += `No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.`
+	// 		return result
+	// 	}
+	// 	return undefined
+	// }
 	return (
 		<div>MainJS</div>
 	);
