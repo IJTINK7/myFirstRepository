c047b5889044da78b12b7210f7177c970cc0ce5f
IJTINK7 <ijtink7@mail.ru>
2023-11-11 14:05:00 +0300

    Adding 142nd Task

--- src/Files/MainJS.tsx
+++ src/Files/MainJS.tsx
@@ -816,6 +816,12 @@ export const MainJS = () => {
 	// function add(a, b){
 	// 	return +a == +b
 	// }
+	// 142 "this" is a problem
+	// function NameMe(first, last) {
+	// 	this.firstName = first;
+	// 	this.lastName = last;
+	// 	return this.name = `${first} ${last}`
+	// }
 	return (
 		<div>MainJS</div>
 	);
